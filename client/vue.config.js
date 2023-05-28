01d2e5b20af2790285a8d3c1baa1bebb53f8c8fb
Ameshiro77 <512065954@qq.com>
2023-05-29 00:41:46 +0800

    关闭了eslint验证

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+
+  lintOnSave: false,
+  devServer: {
+    // 关闭eslint语法验证
+    overlay: {
+      warning: false,
+      errors: false,
+    }
+  },
+
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   transpileDependencies: ['resize-detector'],
