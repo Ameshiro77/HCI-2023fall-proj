afdb4311ca68f3c2cb9b5c3c590086123887d1b0
Ameshiro77 <512065954@qq.com>
2023-06-15 11:46:46 +0800

    加入医疗建议

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -11,7 +11,7 @@
     <e-summary :total="total" :today="today"></e-summary>
 
     <div class="all">
-
+      <!-- 左边图 -->
       <figure class="left-pic">
         <e-charts
           ref="left_line"
@@ -21,12 +21,17 @@
         ></e-charts>
       </figure>
 
-      <div style="margin-top:5%;margin-left:1%">
-        <el-button size="mini" type="primary" icon="el-icon-back" class="go" @click="goback"
+      <!-- 中间地图 -->
+      <div style="margin-top: 5%; margin-left: 1%">
+        <el-button
+          size="mini"
+          type="primary"
+          icon="el-icon-back"
+          class="go"
+          @click="goback"
           >返回
         </el-button>
       </div>
-
       <figure>
         <e-charts
           ref="map"
@@ -37,7 +42,7 @@
         ></e-charts>
       </figure>
 
-
+      <!-- 右边地图 -->
       <figure class="right-pic">
         <e-charts
           ref="right_line"
@@ -47,13 +52,27 @@
         ></e-charts>
       </figure>
     </div>
-    <div>
-      <detect
-      @left_event="updateleftLine" 
-      @right_event="updaterightLine"
-      />
 
+    <!-- 医疗建议 -->
+    <div class="medical-advice">
+      <el-card style="border-radius: 8px; width: 100%">
+        <div style="text-align: left; line-height: 18px">
+          <span> 当前 </span>
+          <span style="font-weight: bold">{{ displayName }}地区</span>
+          <span> 疫情程度：{{ classify }}</span>
+        </div>
+        <div style="text-align: left; line-height: 18px">
+          <span> 医疗建议：{{ medical_advice }}</span>
+        </div>
+      </el-card>
     </div>
+
+    <!-- 检测按钮 -->
+    <div style="position: absolute; right: 4%; top: 0px">
+      <detect @left_event="updateleftLine" @right_event="updaterightLine" />
+    </div>
+
+    <!-- 国内病例 -->
     <div class="section-title">国内病例</div>
     <e-table :data="table"></e-table>
   </div>
@@ -68,8 +87,8 @@ import ESummary from "../components/Summary.vue";
 import { getNameByPinyin, getPinyinByName } from "../data/zhen";
 import buildPredictConfig from "../data/config_predict";
 import advice from "../components/advice.vue";
-import detect from "../components/detect.vue"
-import echarts from 'echarts';
+import detect from "../components/detect.vue";
+import echarts from "echarts";
 const axios = require("axios");
 export default {
   components: {
@@ -88,15 +107,18 @@ export default {
       table: [],
       chinaDayList: null,
       provinceName: "",
+      displayName: "全国",
       initOptions: {
         renderer: "canvas",
       },
       predictList: null, //用于存储预测的折线图数据
       showname: "", //显示省份名还是全国名
-      left_start:0,
-      left_end:100,
-      right_start:0,
-      right_end:100,
+      left_start: 0,
+      left_end: 100,
+      right_start: 0,
+      right_end: 100,
+      classify: "",
+      medical_advice: "",
     };
   },
   methods: {
@@ -105,10 +127,12 @@ export default {
       this.getPredict(params.name);
       //this.$router.push(`/${provincePinyin}`)
     },
+
     //处理返回事件
     goback() {
       this.update(null); //传入null表示返回中国地图
     },
+
     //从后端获取预测数据并展示,传入省份名称
     getPredict(province) {
       let name = getPinyinByName(province);
@@ -147,8 +171,12 @@ export default {
         `,
       });
     },
+
+    // 更新数据
     async update(name) {
-      console.log(name);
+      console.log("name:", name);
+      this.displayName = name;
+      if (this.displayName == null) this.displayName = "全国";
       const {
         updateTime,
         total,
@@ -166,42 +194,70 @@ export default {
       this.table = table;
       this.map = map;
       this.showname = province_name;
+
+      // 根据total获得建议和类别
+      axios({
+        method: "get",
+        url: "http://127.0.0.1:8081/advice",
+        params: {
+          confirm: this.total.confirm,
+          suspect: this.total.suspect,
+          heal: this.total.heal,
+          dead: this.total.dead,
+        },
+      })
+        .then((response) => {
+          console.log(response.data);
+          this.classify = response.data.classify;
+          this.medical_advice = response.data.advice;
+        })
+        .catch(() => {});
     },
+
     //进行左折线图更新
-    updateleftLine(data){
-    let option = this.$refs.left_line.options;
-    if(data.message=='small'&&option.dataZoom[0].end-option.dataZoom[0].start>10){
-      option.dataZoom[0].start +=1;
-      option.dataZoom[0].end -=1;
-    }
-    else if(data.message=='large'&&option.dataZoom[0].end-option.dataZoom[0].start<100){
-      option.dataZoom[0].start-=1;
-      option.dataZoom[0].end +=1;
-    }
-    
-    this.$refs.left_line.option=option;
+    updateleftLine(data) {
+      let option = this.$refs.left_line.options;
+      if (
+        data.message == "small" &&
+        option.dataZoom[0].end - option.dataZoom[0].start > 10
+      ) {
+        option.dataZoom[0].start += 1;
+        option.dataZoom[0].end -= 1;
+      } else if (
+        data.message == "large" &&
+        option.dataZoom[0].end - option.dataZoom[0].start < 100
+      ) {
+        option.dataZoom[0].start -= 1;
+        option.dataZoom[0].end += 1;
+      }
 
-  },
-  //子组件调用该方法进行右折线图缩放扩张
-  updaterightLine(data){
-    let option = this.$refs.right_line.options;
-    if(data.message=='small'&&option.dataZoom[0].end-option.dataZoom[0].start>10){
-      option.dataZoom[0].start +=1;
-      option.dataZoom[0].end -=1;
-    }
-    else if(data.message=='large'&&option.dataZoom[0].end-option.dataZoom[0].start<100){
-      option.dataZoom[0].start-=1;
-      option.dataZoom[0].end +=1;
-    }
-    
-    this.$refs.right_line.option=option;
+      this.$refs.left_line.option = option;
+    },
+    //子组件调用该方法进行右折线图缩放扩张
+    updaterightLine(data) {
+      let option = this.$refs.right_line.options;
+      if (
+        data.message == "small" &&
+        option.dataZoom[0].end - option.dataZoom[0].start > 10
+      ) {
+        option.dataZoom[0].start += 1;
+        option.dataZoom[0].end -= 1;
+      } else if (
+        data.message == "large" &&
+        option.dataZoom[0].end - option.dataZoom[0].start < 100
+      ) {
+        option.dataZoom[0].start -= 1;
+        option.dataZoom[0].end += 1;
+      }
 
-},
+      this.$refs.right_line.option = option;
+    },
   },
+
   async created() {
     let province = this.$route.path.substr(1);
     this.provinceName = getNameByPinyin(province);
-    console.log(this.provinceName);
+    console.log("name:", this.provinceName);
     const { updateTime, total, map, table, chinaDayList, today } =
       await buildMapData(this.provinceName);
 
@@ -222,7 +278,7 @@ export default {
   width: 30%;
   border: 1px solid #999;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-  margin-left:1%;
+  margin-left: 1%;
 }
 .right-pic {
   width: 30%;
@@ -230,12 +286,17 @@ export default {
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
   /* margin-right:1%; */
 }
+
+.medical-advice {
+  padding: 10px;
+  margin-top: 20px;
+}
+
 .all {
   margin-top: 10px;
   display: flex;
   width: 100%;
 }
 .go {
-
 }
 </style>
--- server/app.py
+++ server/app.py
@@ -7,6 +7,8 @@ from werkzeug.utils import secure_filename
 import pandas as pd
 # ==================================
 
+
+
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__,
@@ -41,13 +43,48 @@ def getData(province_name):
 """
 @app.route('/predict', methods=['GET'])
 def predict():
-
     name = request.values.get('province_name')  # 前端传一个省份名字的参数
-
     # ========= 修改部分 ========== #
     data = getData(name)   # 需要自定义getData函数，根据城市名，传递一个数组：该数组包含这个城市的预测值（也可以加上已有值）
     return jsonify(data)
 
+"""
+根据四个人数数据 确定严重程度和医疗建议
+"""
+@app.route('/advice', methods=['GET'])
+def advice():
+    confirm = request.values.get('confirm')
+    print("==========",type(confirm),confirm)
+    suspect = request.values.get('suspect')
+    heal = request.values.get('heal')
+    dead = request.values.get('dead')
+    # cf_a = 20520/31
+    # sp_a = 23214/31
+    per = (0.6 * int(confirm) / 20520.0) + (0.4 * int(suspect) / 23214.0)
+    classify = ''
+    advice = ''
+    if per < 0.005:
+        classify = '比较轻微'
+        advice = "当前疫情程度较轻微，建议大家继续保持良好的个人卫生习惯。请经常洗手，使用洗手液或肥皂，尤其在接触公共场所后或打喷嚏、咳嗽后。同时，佩戴口罩有助于减少病毒传播，特别是在人群密集的地方。避免不必要的聚集和旅行，尽量在户外保持社交距离。关注当地卫生部门的指导和最新疫情信息，确保获取准确可靠的消息来源。"
+    elif per < 0.01:
+        classify = '轻微'
+        advice = "当前疫情程度为轻微，但仍需保持警惕。请坚持良好的个人卫生习惯，包括经常洗手、佩戴口罩。尽量避免接触潜在传播源，遵守当地卫生部门的指引和规定。减少外出，特别是人群聚集的场所。如果出现不适症状，应及时就医并告知医生最近的活动历史。密切关注疫情动态，遵循专家和当地卫生部门的建议。"
+    elif per < 0.05: 
+        classify = '中度'
+        advice = "当前疫情程度为中度，建议加强防护措施。请严格遵守卫生规范，如勤洗手、佩戴口罩。减少外出和接触他人，尽量居家工作。遵循医疗专家的建议，如有不适症状应及时就医，并遵守治疗方案。注意与家人保持良好的卫生习惯，避免共用个人物品。关注疫情信息更新，密切配合当地卫生部门的指示。"
+    elif per < 0.1: 
+        classify = '较为严重'
+        advice = "当前疫情程度比较严重，需要加强防护措施。请高度警惕，严格遵守卫生规范，如勤洗手、佩戴口罩。限制社交接触，避免不必要的出行，尽量居家工作。关注疫情信息更新，及时获取准确的消息来源。配合政府和卫生部门的指示，遵守防疫措施，如隔离、检测等，以保护自己和他人的健康。"
+    elif per < 0.15: 
+        classify = '严重'
+        advice = "当前疫情程度比较严重，需要加强防护措施。请高度警惕，严格遵守卫生规范，如勤洗手、佩戴口罩。限制社交接触，避免不必要的出行，尽量居家工作。关注疫情信息更新，及时获取准确的消息来源。配合政府和卫生部门的指示，遵守防疫措施，如隔离、检测等，以保护自己和他人的健康。"
+    else:
+        classify = '非常严重'
+        advice = "当前疫情程度非常严重，请立即采取紧急措施保护自己和他人。严格遵守防护措施，包括频繁洗手、全面佩戴口罩，避免与他人近距离接触。避免不必要的社交活动和出行，尽量居家工作。严格遵守医疗专家的治疗方案。紧急时刻，密切关注疫情动态，遵从政府和卫生部门的紧急指示和命令。团结合作，共同努力，共渡难关。"
+    return jsonify({"classify":classify,"advice":advice})
+
+
+    
 #================================================#
 #                 主 函 数 入 口                 #
 #================================================#
@@ -57,4 +94,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+   app.run(debug=True, host='0.0.0.0', port=8081)
